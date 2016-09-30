75d4bac03c3bdbd5474136a146ef557a00f4c24b
Antoinette Smith <antoinette@primeacademy.io>
2016-09-30 08:48:44 -0500

    no no double quotes

--- server/app.js
+++ server/app.js
@@ -1,19 +1,19 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var path = require("path");
-var data = require("./public/data/rho");
+var path = require('path');
+var data = require('./public/data/rho');
 
-app.set("port", (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-app.get("/data", function(req,res){
+app.get('/data', function (req, res) {
     res.send(data);
-});
+  });
 
-app.get("/*", function(req,res){
-    var file = req.params[0] || "views/index.html";
-    res.sendFile(path.join(__dirname, "/public/", file));
-});
+app.get('/*', function (req, res) {
+    var file = req.params[0] || 'views/index.html';
+    res.sendFile(path.join(__dirname, '/public/', file));
+  });
 
-app.listen(app.get("port"), function(){
-    console.log("Server up and running on Port", 5000);
-});
+app.listen(app.get('port'), function () {
+    console.log('Server up and running on port', app.get('port'));
+  });
