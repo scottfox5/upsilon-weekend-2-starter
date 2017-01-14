b1f700e8e357b2c465a518174a25d460748c5cd0
Scott Fox <scottfox5@gmail.com>
2017-01-14 11:36:04 -0600

    Built basic functionality for project: when buttons are clicked data is retrieved and  DOM is appended with information for one student.

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,30 +1,50 @@
 //Client-side code
+var personCounter = 0;
+
 $(function(){
 
-  $('button').click(getPeople);//When button is clicked, getPeeps is run and all info is collected and appended to DOM
+  $('#nextButton').click(nextPerson);//When button is clicked, getPeople is run and all info is collected and appended to DOM
+  $('#previousButton').click(previousPerson);//
 
 });
 
 function appendDom(person){
-  var $personDiv = $('<div class ="person"></div>');//Setting jQuery variable of div to be appended
+  var $personDiv = $('<div class ="person" id='+personCounter+'></div>');//Setting jQuery variable of div to be appended
   $personDiv.append('<p>' + person.githubUserName + '</p>');//Adding property information to append to div
   $personDiv.append('<p>' + person.name + '<p/>');
   $personDiv.append('<p>' + person.shoutout + '<p/>');
   $('#ajax-data').append($personDiv);//Appending properties of person object to DOM
 };
 
-function getPeople(){
+function nextPerson(){
+  getPerson();
+  //removePeople();
+  personCounter++;
+  // if (personCounter = 17){//Resetting personCounter for nextButton so clicking on last will bring to first
+  //   personCounter = 0;
+  // }
+}
 
+function previousPerson(){
+  getPerson();
+  //removePeople();
+  personCounter--;
+  // if (personCounter = -1){//Resetting personCounter for previousButton so clicking on first will bring to last
+  //   personCounter = 16;
+  // }
+}
+
+// function removePeople(){//Trying to remove all people with class of person except the one with id of current personCounter
+//   (".person:not(#"+personCounter+")").remove();
+// }
+
+function getPerson(){
   $.ajax({
     type: "GET",
     url: "/data",
     success: function(response){
-      console.log(response);//Verifying response is returning data
-
-      response.forEach(function(person){//Looping through each person object and using property values to append DOM
-      appendDom(person);
-      });
+      console.log(response[personCounter]);
+      appendDom(response[personCounter]);
     }
   });
-
 }
